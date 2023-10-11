d94e0fbfd75e4588692788da4e28671f0c037306
Alba Zefi <alba.zefii@gmail.com>
2023-10-11 13:01:51 +0200

    First Commit
    First Date: 11/10/2023

---
+++ js/questions.js
@@ -0,0 +1,55 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Cili eshte zhanri juaj i preferuar?",
+    options: [
+      "Anime",
+      "Action",
+      "Family",
+      "Romance",
+      "Crime",
+      "Horror",
+      "Thriller",
+      "Comedies",
+      "Documentary"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What sub-genres well perfer?",
+    
+    options: [
+      "Crime Thriller",
+      "Disaster Thriller",
+      "Psychological Thriller",
+      "Techno Thriller"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Select the language",
+   
+    options: [
+      "Shqip",
+      "English",
+      "French",
+      "Japanese"
+    ]
+  },
+    ,
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
